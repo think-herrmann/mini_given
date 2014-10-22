ea95ed961c812dce3acfaae49ed2630c9d20611f
Andrew Swerlick <andrew.swerlick@hbdi.com>
2014-10-21 21:40:39 -0400

    setting up mini_given so that including it automatically extends minitest spec

--- lib/mini_given.rb
+++ lib/mini_given.rb
@@ -1,5 +1,8 @@
 require "mini_given/version"
+require 'minitest/spec'
 
 module MiniGiven
   autoload :Spec, 'mini_given/spec'
 end
+
+MiniTest::Spec.send(:extend, MiniGiven::Spec)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,5 @@
-require 'mini_given'
 require 'minitest/unit'
+require 'mini_given'
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'byebug'
-
-class MiniTest::Spec
-  extend MiniGiven::Spec
-end
